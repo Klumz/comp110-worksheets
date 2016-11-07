2a4361ac574d744396d40d74be3ceb507381f75b
Michail Karakasis <michailmore_k@hotmail.com>
2016-11-07 00:14:12 +0000

    Update oxo.py

--- worksheet_D/oxo.py
+++ worksheet_D/oxo.py
@@ -1,5 +1,5 @@
 class OxoBoard:
-    def __init__(self, x, y):                           #Initialize and define board
+    def __init__(self, x, y):               #Initialize and define board
         self.board = {(0, 0): 0, (0, 1): 0, (0, 2): 0, (1, 0): 0, (2, 0): 0, (1, 1): 0, (2, 2): 0, (1, 2): 0, (2, 1): 0}
 
     def get_square(self, x, y):             #Returns current value of the current position
